2cbb68b56d5bd8373d2e5d80bedf3dcea863050c
iamdami <50016477+iamdami@users.noreply.github.com>
2020-10-30 17:59:57 +0900

    arrayAscending.py

--- array/arrayAscending.py
+++ array/arrayAscending.py
@@ -7,32 +7,32 @@ def swap(arr, x, y):
     
     
 def ascendingSort(arr, N):
-    # 원래 저장되어 있던 값 출력
+    # Print the original saved value
     print("Original arr: ", arr)
     n = N - 1
-    # 카운트 값 초기화
+    # Initialize count value
     count = 0
     j = 0
     while (n):
-        # 연산 횟수
+        # Number of operations
         print("n: ", n)
-        # 누적 연산 값 저장될 변수
+        # Variable to store the accumulated operation value
         temp = 0
         for i in range(1, n + 1):
 
-            # 배열 원소 내 값 비교 수행
+            # Perform comparison of values within array elements
             if (arr[i - 1] > arr[i]):
                 swap(arr, i - 1, i)
                 temp = i
                 j = j + 1
-                # 값 비교 연산 횟수와 데이터 출력
+                # Print the Value comparison operation count and the data
             print(i, " ", arr)
         n = temp
-    # 총 변환 횟수 출력
+    # Print the Total conversion count
     print('Total numbers of changes: ', j)
 
 
-with open('C:/Users/Dami/Desktop/python/3.txt', 'r') as ascending:
+with open('PutFileName.txt', 'r') as ascending:
     arr = [int(float(i)) for i in ascending.read().split(',')]
 
 a = np.array(arr)
