9747d5a8d9b1b4bd07ea52c7382435bba96c1c82
iamdami <50016477+iamdami@users.noreply.github.com>
2020-11-05 14:22:51 +0900

    Update arrayAscending.py

--- array/arrayAscending.py
+++ array/arrayAscending.py
@@ -1,42 +1,40 @@
+# array 사용하기 위해 numpy import
 import numpy as np
 
+
+# 값 교환해주는 함수 정의
 def swap(arr, x, y):
-    temp = arr[x]
-    arr[x] = arr[y]
-    arr[y] = temp
-    
-    
-def ascendingSort(arr, N):
-    # Print the original saved value
-    print("Original arr: ", arr)
-    n = N - 1
-    # Initialize count value
-    count = 0
-    j = 0
+    temp = arr[x]  # 임시 저장소(temp)에 arr[x]값 넣어줌
+    arr[x] = arr[y]  # arr[x]에 arr[y]값 넣어줌
+    arr[y] = temp  # arr[y]에 임시 저장소(temp)값 넣어줌
+
+
+# Bubble sort 오름차순 정렬 함수 정의
+def ascendingSort(arr, N):  # 배열, 배열길이값
+    n = N - 1  # 배열 길이값 - 1
+    j = 0  # 1회전 할 때마다 카운트 해 줄 변수 초기화
     while (n):
-        # Number of operations
-        print("n: ", n)
-        # Variable to store the accumulated operation value
-        temp = 0
+        print("n: ", n)  # 연산 횟수
+        temp = 0  # 누적 연산 값 저장될 변수
         for i in range(1, n + 1):
-
-            # Perform comparison of values within array elements
+            # 첫 번째 값과 그 다음 값 비교
             if (arr[i - 1] > arr[i]):
-                swap(arr, i - 1, i)
-                temp = i
-                j = j + 1
-                # Print the Value comparison operation count and the data
-            print(i, " ", arr)
-        n = temp
-    # Print the Total conversion count
-    print('Total numbers of changes: ', j)
+                swap(arr, i - 1, i)  # swap함수
+                temp = i  # for loop 수행될 때마다 값 저장
+                j = j + 1  # 1회전 할 때마다 +1
+            print(i, " ", arr)  # 값 비교 연산 횟수, 데이터 출력
+        n = temp  # temp에 저장된 값과 n값 같으면
+    print('Turns of changes: ', j)  # 총 회전 횟수 출력
 
 
-with open('PutFileName.txt', 'r') as ascending:
-    arr = [int(float(i)) for i in ascending.read().split(',')]
+# 파일 읽어오기
+with open('C:/Users/Dami/Desktop/python/3.txt', 'r') as ascending:
+    #  ascending이라는 이름으로 읽어옴(r)
+    data = [int(float(i)) for i in ascending.read().split(',')]
+    # ','를 구분자로 파일 토큰 분류한 값(float)을 int로 변환해 data에 저장
 
-a = np.array(arr)
+print("Original: ", data)  # data값 출력
 
-print("original: ",arr)
+arr = np.array(data)  # numpy.array 함수 사용해 배열 값 arr에 저장
 
-ascendingSort(arr, len(arr))
+ascendingSort(arr, len(arr))  # 배열에 대해 오름차순 정렬 수행
