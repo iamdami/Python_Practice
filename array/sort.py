48c5cdb422f304912365d39aee4475c1ba6fd1a3
iamdami <50016477+iamdami@users.noreply.github.com>
2020-11-06 19:00:28 +0900

    Update sort.py

--- array/sort.py
+++ array/sort.py
@@ -1,16 +1,14 @@
 # numpy import
 import numpy as np
 
-# file read
-with open('putFileName.txt', 'r') as ascending:
-    arr = [int(float(i)) for i in ascending.read().split(',')]
+# 파일 읽어오기
+with open('C:/putFileName.txt', 'r') as ascending:
+    # ascending이라는 이름으로 읽어옴(r)
+    data = [int(float(i)) for i in ascending.read().split(',')]
+    # ','를 구분자로 파일 토큰 분류한 값(float)을 int로 변환해 data에 저장
 
-# Save in variable 'arr' in array format
-arr = np.array(arr)
+print("Original array: ", data)  # data값 출력
+arr = np.array(data)  # numpy.array 함수 사용해 배열 값 arr에 저장
 
-print("Original array: ", arr)
-
-# sorting
-s = np.sort(arr)
-
-print("After sort: ", s)
+s = np.sort(arr)  # np.sort 함수 이용해 정렬한 값 변수 s에 넣어줌
+print("After sort: ", s)  # 정렬한 값 출력
