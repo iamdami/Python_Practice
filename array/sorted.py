abc5c7d9f41344517fe6ca3c24d135492e4732b0
iamdami <50016477+iamdami@users.noreply.github.com>
2020-11-20 00:00:55 +0900

    sorted.py

---
+++ array/sorted.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+org_array = np.array([2, 1, 0, 7])
+print('org array :', org_array)
+
+sort_array = sorted(org_array)
+
+print('sorted() :', sort_array)
+print('org array after sort :', org_array)
