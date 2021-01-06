623afdcdd0416df8ce827ffd06f83d636d0ce88e
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-07 02:55:43 +0900

    join.py

---
+++ PyForML/join.py
@@ -0,0 +1,17 @@
+# General code
+
+colors = ["a", "b", "c", "d", "e"]
+result = ""
+
+for s in colors:
+    result += s
+
+print(result)
+
+
+# pythonic code
+
+colors = ["red", "blue", "green", "yellow"]
+result = "".join(colors)
+
+print(result)
