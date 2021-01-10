d5277068be29c583e353c80562b5caa7f676218e
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-10 19:48:06 +0900

    Update NestedForLoop_if.py

--- PyForML/NestedForLoop_if.py
+++ PyForML/NestedForLoop_if.py
@@ -3,6 +3,7 @@ case_2 = ["D", "E", "A"]
 
 result = [i+j for i in case_1 for j in case_2]
 print(result)
+# ['AD', 'AE', 'AA', 'BD', 'BE', 'BA', 'CD', 'CE', 'CA']
 
 
 # add if
@@ -10,3 +11,4 @@ result = [i+j for i in case_1 for j in case_2 if not(i==j)]
 result.sort()
 
 print(result)
+# ['AD', 'AE', 'BA', 'BD', 'BE', 'CA', 'CD', 'CE']
