0b438f478fbbe90fe356352770114175ae5d6444
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-10 18:55:40 +0900

    join1.py

---
+++ PyForML/join1.py
@@ -0,0 +1,14 @@
+colors = ['red', 'blue', 'green', 'yellow']
+result = ''.join(colors) 
+result 
+# 'redbluegreenyellow' 
+
+
+result = ', '.join(colors) 
+result 
+# 'red, blue, green, yellow' 
+
+
+result = '-'.join(colors) 
+result 
+# 'red-blue-green-yellow'
