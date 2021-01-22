e9c46cdeb22c5d8dc6e99d235067feb1338651d0
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-22 23:59:18 +0900

    Create applyFont.py

---
+++ tkinter/applyFont.py
@@ -0,0 +1,7 @@
+from tkinter import *
+rt = Tk()
+rt.geometry("500x300")
+rt.title("title")
+lbl = Label(rt, text="I love my cat ChaCha", font=("Arial Bold", 20))
+lbl.grid(column=0, row=0)
+rt.mainloop()
