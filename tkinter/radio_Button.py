c85e5a3a95998293130f9683ce6345b2003aea21
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-25 17:37:36 +0900

    Update radio_Button.py

--- tkinter/radio_Button.py
+++ tkinter/radio_Button.py
@@ -1,12 +1,12 @@
 from tkinter import *
 
 rt = Tk()
-rt.title("과일 선택")
+rt.title("select fruit")
 selected = IntVar()
 
-rad1 = Radiobutton(rt, text='포도', value=1, variable=selected)
-rad2 = Radiobutton(rt, text='사과', value=2, variable=selected)
-rad3 = Radiobutton(rt, text='딸기', value=3, variable=selected)
+rad1 = Radiobutton(rt, text='grape', value=1, variable=selected)
+rad2 = Radiobutton(rt, text='apple', value=2, variable=selected)
+rad3 = Radiobutton(rt, text='berry', value=3, variable=selected)
 
 emptyLabel = Label(rt, text=" ")
 
@@ -20,7 +20,7 @@ printLabel = Label(rt, text=selected.get())
 printLabel =grid(column=0, row=3)
 
 
-btn = Button(rt, text="클릭해방", command = clicked)
+btn = Button(rt, text="click", command = clicked)
 btn.grid(column=1, row=2)
 
 rt.mainloop()
