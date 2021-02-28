e85074d7edc06f716507c632b8908d11c09a206a
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-02-28 17:18:03 +0900

    Create Multiple_loops.py

---
+++ Reduce-Indent/Multiple_loops.py
@@ -0,0 +1,11 @@
+# Reduce indents with quotient and remainder
+
+for i in range(N):
+  for j in range(N):
+    for k in range(N):
+      # i, j, k process
+      
+      
+for num in range(N ** 3):
+  i, j, k = num // (N * N), num // N % N, num % N
+  # i, j, k process
