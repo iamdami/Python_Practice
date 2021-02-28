7a614c03afcd924f822c7832cae9925d1e5b6b33
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-02-28 17:25:38 +0900

    Create When_else_is_the_last_conditional_statement.py

---
+++ Reduce-Indent/When_else_is_the_last_conditional_statement.py
@@ -0,0 +1,15 @@
+def fun(x):
+  if x :
+    return True
+  else :
+    return False
+  
+  
+def fun(x):
+  if x return True
+  return False
+
+
+def fun(x):
+  return True if x else False
+  # ternary operator
