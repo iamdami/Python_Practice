7691d352de5cd7bf1edec366acaf68ca0410be44
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-12-16 21:24:06 +0900

    Create stocks_nan.py

---
+++ 모두의데이터과학/code/practice/stocks_nan.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+sap = np.array(["MMM", "ABT", "ABBV", "ACN", "ACE", "ATVI", "ADBE", "ADT"])
+
+stocks = np.array(
+    [140.49, 0.97, 40.68, 41.53, 55.7, 57.21, 98.2, 99.19, 109.96, 111.47, 35.71, 36.27, 87.85, 89.11, 30.22, 30.91])
+print(stocks)
+print("\n")
+
+stocks_reshape_t = stocks.reshape(8, 2).T
+print(stocks_reshape_t)
+print("\n")
+
+stocks_reshape_t[1, 0] = np.nan
+print("새 주식가격이 결측치라고 가정")
+print(np.isnan(stocks_reshape_t))
+print(type(np.isnan(stocks_reshape_t)))
+print("\n")
+
+stocks_reshape_t[np.isnan(stocks_reshape_t)] = 0
+print(type(stocks_reshape_t[np.isnan(stocks_reshape_t)]))
+print(stocks_reshape_t[np.isnan(stocks_reshape_t)])
+print("결측치 수정")
+print(stocks_reshape_t)
