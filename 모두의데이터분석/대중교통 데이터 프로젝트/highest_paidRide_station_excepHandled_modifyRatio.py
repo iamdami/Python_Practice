90fb47013c1a9bb42b2351c535a45f117a25ba53
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-12-24 19:41:41 +0900

    Create highest_paidRide_station_excepHandled_modifyRatio.py

---
+++ 모두의데이터분석/대중교통 데이터 프로젝트/highest_paidRide_station_excepHandled_modifyRatio.py
@@ -0,0 +1,14 @@
+import csv
+f = open('subwayfee.csv')
+data = csv.reader(f)
+next(data)
+mx = 0
+rate = 0
+for row in data :
+    for i in range(4,8) :
+        row[i] = int(row[i])
+    if row[6] !=0 and (row[4] + row[6]) >100000 :
+        rate = row[4] / (row[4] + row[6])
+        if rate > mx :
+            mx = rate
+            print(row, round(rate,2))
