64ae27e4d92ceb0632db899196797bc62229ae6d
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-12-24 19:49:42 +0900

    Create piechartSave_paid_n_freeRide_ratio_allStation_upVer.py

---
+++ 모두의데이터분석/대중교통 데이터 프로젝트/piechartSave_paid_n_freeRide_ratio_allStation_upVer.py
@@ -0,0 +1,17 @@
+import csv
+import matplotlib.pyplot as plt
+f = open('subwayfee.csv')
+data = csv.reader(f)
+next(data)
+label = ['유임승차','유임하차','무임승차','무임하차']
+c = ['#14CCC0', '#389993', '#FF1C6A', '#CC14AF']
+plt.rc('font', family = 'Malgun Gothic')
+for row in data :
+    for i in range(4,8) :
+        row[i] = int(row[i])
+    plt.figure(dpi = 300)
+    plt.title(row[3] + ' ' + row[1])
+    plt.pie(row[4:8], labels = label, colors = c, autopct = '%1.f%%')
+    plt.axis('equal')
+    plt.savefig(row[3] + ' ' + row[1] + '.png')
+    plt.show()
